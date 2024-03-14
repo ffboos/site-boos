a7961ad3892a053c2c1b2aff60a16f7d64130fba
Markus Barenhoff <mbarenh@alios.org>
2024-03-14 14:13:24 +0100

    Prepare release boos-2024.1.0

--- image-customization.lua
+++ image-customization.lua
@@ -1,21 +1,29 @@
-packages {'iwinfo'}
+packages {'iwinfo', 'tcpdump' }
 
 features {
 	'autoupdater',
 	'config-mode-domain-select',
+	'config-mode-outdoor',
 	'config-mode-geo-location-osm',
-	'config-mode-mesh-vpn'
+	'config-mode-mesh-vpn',
+	'config-mode-contact-info',
 	'ebtables-filter-multicast',
 	'ebtables-filter-ra-dhcp',
 	'ebtables-limit-arp',
 	'ebtables-source-filter',
+	'ffmuc-mesh-vpn-wireguard-vxlan',
 	'mesh-batman-adv-15',
-	'mesh-vpn-fastd',
+	'mesh-wireless-sae',
+	'mesh-vpn-wireguard',
+	'node-info',
+	'neighbour-info',
 	'respondd',
 	'status-page',
+	'status-page-mesh-batman-adv',
 	'web-advanced',
-	'web-private-wifi'
-	'web-wizard'
+	'web-network',
+	'web-private-wifi',
+	'web-wizard',
 }
 
 if not device_class('tiny') then
