3f14ca557af85b83c92bff09ec743b52040e1246
Markus Barenhoff <mbarenh@alios.org>
2024-03-17 08:10:36 +0100

    fixed image-custom

--- image-customization.lua
+++ image-customization.lua
@@ -3,13 +3,17 @@ packages {'iwinfo'}
 features {
 	'autoupdater',
 	'config-mode-domain-select',
+	'config-mode-outdoor'
 	'config-mode-geo-location-osm',
 	'config-mode-mesh-vpn',
+	'config-mode-contact-info',
 	'ebtables-filter-multicast',
 	'ebtables-filter-ra-dhcp',
 	'ebtables-limit-arp',
 	'ebtables-source-filter',
 	'mesh-batman-adv-15',
+	'mesh-wireless-sae',
+	'mesh-vpn-wireguard',
 	'respondd',
 	'status-page',
 	'web-advanced',
