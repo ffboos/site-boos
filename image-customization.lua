cd564f698b994e7de1be807478a6bcf5c6bd9b89
Markus Barenhoff <mbarenh@alios.org>
2024-03-18 20:41:46 +0100

    [Bug] Broken / missing features.

--- image-customization.lua
+++ image-customization.lua
@@ -1,17 +1,28 @@
-packages {'iwinfo', 'tcpdump' }
+packages {
+	'ff-ap-timer',
+	'ff-web-ap-timer',
+	'ffac-autoupdater-wifi-fallback',
+	'ffac-change-autoupdater',
+	'ffac-ssid-changer',
+	'ffmuc-ipv6-ra-filter',
+	'ffmuc-mesh-vpn-wireguard-vxlan',
+	'iwinfo',
+	'respondd-module-airtime',
+	'tcpdump',
+	'ffmuc-mesh-vpn-wireguard-vxlan',
+}
 
 features {
 	'autoupdater',
 	'config-mode-domain-select',
-	'config-mode-outdoor',
 	'config-mode-geo-location-osm',
 	'config-mode-mesh-vpn',
 	'config-mode-contact-info',
+	'config-mode-outdoor',
 	'ebtables-filter-multicast',
 	'ebtables-filter-ra-dhcp',
 	'ebtables-limit-arp',
 	'ebtables-source-filter',
-	'ffmuc-mesh-vpn-wireguard-vxlan',
 	'mesh-batman-adv-15',
 	'mesh-wireless-sae',
 	'mesh-vpn-wireguard',
